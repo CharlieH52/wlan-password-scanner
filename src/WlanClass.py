20c9e1634651c07a28baa133689365856e1a2528
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-05-05 14:58:38 -0600

    [REF] All code changed for manage JSON files and continuous integration

---
+++ src/WlanClass.py
@@ -0,0 +1,20 @@
+import json
+
+class Wlan:
+    def __init__(self, ssid, password, upDate):
+        self.ssid = ssid
+        self.password = password
+        self.upDate = upDate
+
+    def to_dict(self):
+        return {
+            'ssid': self.ssid,
+            'password': self.password,
+            'upDate': self.upDate
+        }
+
+    @staticmethod
+    def load_from_json(file_path):
+        with open(file_path, "r") as jsonFile:
+            data = json.load(jsonFile)
+            return [Wlan(**item) for item in data]
