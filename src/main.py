1893a3c629f7d841fcfece15646cec5fa3d1d357
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-10-10 11:02:40 -0600

    REF: JSON local storage deleted from the main program

--- src/main.py
+++ src/main.py
@@ -1,17 +1,14 @@
-from services.data_management import Writer
 from entities.wlan_class import Wlan
 from services.wlan_scanner import WLANScanner
 from repository.wlan_repository import WlanSqlManagement
 
 ws = WLANScanner()
-wr = Writer()
 
 def run():
     ssid_list = ws.get_ssid_profiles_list()
     for wlan in ssid_list:
         pssw = ws.get_ssid_password(wlan)
-        new_wlan = Wlan(ssid=wlan, password=pssw)
-        # wr.save_data_in_file(new_wlan.to_dict())    
+        new_wlan = Wlan(ssid=wlan, password=pssw)    
 
         WlanSqlManagement(new_wlan).post_wlan()
 
